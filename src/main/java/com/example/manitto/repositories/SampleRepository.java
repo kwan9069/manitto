9a9c7f7ea5a055ff8eab009f4416e21800c96f49
zezeg2 <whdgus003@gmail.com>
2023-01-19 17:54:55 +0900

    page structure init

--- src/main/java/com/example/manitto/repositories/SampleRepository.java
+++ src/main/java/com/example/manitto/repositories/SampleRepository.java
@@ -1,7 +1,9 @@
 package com.example.manitto.repositories;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @Mapper
 public class SampleRepository {
 }
